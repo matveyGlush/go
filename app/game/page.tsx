5825b4ace7eb661eca28787024c34d254fc2cd54
Matvey Glushko <matveyglushkospb@gmail.com>
2025-02-21 13:35:34 +0300

    get data from helios db

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,14 +1,23 @@
-"use client"
 import CustomLayout from '@/components/CustomLayout';
 import GameBoard from '@/components/GameBoard';
+import { getData } from '../_lib/data';
 
-export default function Game() {
+export default async function Game() {
+
+  let dataUsers
+
+  try {
+    dataUsers = await getData()
+    console.log(dataUsers)
+  } catch (error) {
+    console.error('Failed to fetch data:', error);
+  }
+
 
   return (
-    <>
-      <CustomLayout className='flex justify-center items-center overflow-hidden'>
-        <GameBoard/>
-      </CustomLayout>
-    </>
+    <CustomLayout className='flex justify-center items-center overflow-hidden'>
+      <div>{JSON.stringify(dataUsers)}</div>
+      <GameBoard />
+    </CustomLayout>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // webpack: (config, context) => {
-  //   config.module.rules.push({
-  //     test: /\.node$/,
-  //     loader: "node-loader",
-  //   });
-  //   return config;
-  // },
+  webpack: (config, context) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: "node-loader",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
