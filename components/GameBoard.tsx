f3198304391297f7a98cc282162cb1ccd4912859
Matvey Glushko <matveyglushkospb@gmail.com>
2025-03-17 22:46:59 +0300

    key

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -70,7 +70,7 @@ export default function GameBoard({ size = 9, crossings = [], playerId, color, t
         console.log('found coordinate' + x + '.' + crossing.x + ';' + y + '.' + crossing.y)
         console.log(crossing.player_color)
         return (
-          <Rock gameSizes={gameSizes} coordinates={[x, y]} stoneColor={crossing.player_color} argIsVisible={true}/>
+          <Rock key={crossing.player_color+x+y} gameSizes={gameSizes} coordinates={[x, y]} stoneColor={crossing.player_color} argIsVisible={true}/>
         )
       }
     })
