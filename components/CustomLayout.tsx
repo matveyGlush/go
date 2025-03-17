9a3f96e51ea25842c26bb07da79a378ad81af2cd
Matvey Glushko <matveyglushkospb@gmail.com>
2025-03-17 12:45:06 +0300

    fix layout

--- components/CustomLayout.tsx
+++ components/CustomLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useEffect } from "react";
+import { ReactNode, Suspense, useEffect } from "react";
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 import HideOnScroll from "./HideOnScroll";
@@ -28,30 +28,32 @@ export default function CustomLayout({ children, className }: { children: ReactN
   }
 
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col">
-      <HideOnScroll>
-        <header className="fixed top-0 left-0 w-full bg-black text-white z-50">
-          <div className="container mx-auto max-w-4xl flex justify-between items-center py-4 px-6">
-            <Link href="/">
-              <div className="text-xl font-bold">Игра&nbsp;Го</div>
-            </Link>
-            {pathname !== "/account" && pathname !== "/game" && (
-              <CustomLink href={isAuth === 'in' ? 'account' : 'auth'} theme="dark">Аккаунт</CustomLink>
-            )}
-            {pathname === "/game" && (
-              <CunstomButton theme="dark" onClickFunc={() => handleGameExit()}>Выйти из игры</CunstomButton>
-            )}
-          </div>
-        </header>
-      </HideOnScroll>
+    <Suspense>
+      <div className="bg-gray-100 min-h-screen flex flex-col">
+        <HideOnScroll>
+          <header className="fixed top-0 left-0 w-full bg-black text-white z-50">
+            <div className="container mx-auto max-w-4xl flex justify-between items-center py-4 px-6">
+              <Link href="/">
+                <div className="text-xl font-bold">Игра&nbsp;Го</div>
+              </Link>
+              {pathname !== "/account" && pathname !== "/game" && (
+                <CustomLink href={isAuth === 'in' ? 'account' : 'auth'} theme="dark">Аккаунт</CustomLink>
+              )}
+              {pathname === "/game" && (
+                <CunstomButton theme="dark" onClickFunc={() => handleGameExit()}>Выйти из игры</CunstomButton>
+              )}
+            </div>
+          </header>
+        </HideOnScroll>
 
-      <main className={`container mx-auto max-w-4xl pt-24 px-4 flex-grow text-black ${className}`}>
-        <div>{children}</div>
-      </main>
+        <main className={`container mx-auto max-w-4xl pt-24 px-4 flex-grow text-black ${className}`}>
+          <div>{children}</div>
+        </main>
 
-      <footer className="text-center py-4 bg-white">
-        <Link href={'https://github.com/matveyGlush/go'} className="text-gray-600 text-sm underline">github&nbsp;matveyGlush</Link>
-      </footer>
-    </div>  
+        <footer className="text-center py-4 bg-white">
+          <Link href={'https://github.com/matveyGlush/go'} className="text-gray-600 text-sm underline">github&nbsp;matveyGlush</Link>
+        </footer>
+      </div>  
+    </Suspense>
   );
 }
