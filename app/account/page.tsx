5b77330c8789f72ed6b611551508bd8f39a3aae5
Matvey Glushko <matveyglushkospb@gmail.com>
2025-03-17 12:35:10 +0300

    fix account

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,7 +3,7 @@ import CunstomButton from "@/components/CustomButton";
 import CustomLayout from "@/components/CustomLayout";
 import { getUserStats, logout } from "../_lib/data";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function Account() {
 
@@ -43,14 +43,16 @@ export default function Account() {
     }, [])
 
   return(
-    <CustomLayout>
-      <h2 className="mb text-2xl font-bold text-center">ПРОЦЕНТ ПОБЕД</h2>
-      {user && <h3 className="mb-7 text-9xl font-bold text-center">{Math.round((user?.wins / user?.total_games) * 100)}</h3>}
-      <div className="flex justify-between md:flex-row flex-col md:gap-5 md:mx-auto md:justify-center">
-        <CunstomButton className="px-4 py-2 text-xs mb-6 md:h-16" onClickFunc={() => handleLogout()}>Выйти</CunstomButton>
-        <CunstomButton className="px-4 py-2 text-xs mb-2 md:h-16">Сброс пароля</CunstomButton>
-        <CunstomButton className="px-4 py-2 bg-red-800 text-white mb-4 text-xs md:h-16">Удалить аккаунт</CunstomButton>
-      </div>
-    </CustomLayout>
+    <Suspense>
+      <CustomLayout>
+        <h2 className="mb text-2xl font-bold text-center">ПРОЦЕНТ ПОБЕД</h2>
+        {user && <h3 className="mb-7 text-9xl font-bold text-center">{Math.round((user?.wins / user?.total_games) * 100)}</h3>}
+        <div className="flex justify-between md:flex-row flex-col md:gap-5 md:mx-auto md:justify-center">
+          <CunstomButton className="px-4 py-2 text-xs mb-6 md:h-16" onClickFunc={() => handleLogout()}>Выйти</CunstomButton>
+          <CunstomButton className="px-4 py-2 text-xs mb-2 md:h-16">Сброс пароля</CunstomButton>
+          <CunstomButton className="px-4 py-2 bg-red-800 text-white mb-4 text-xs md:h-16">Удалить аккаунт</CunstomButton>
+        </div>
+      </CustomLayout>
+    </Suspense>
   )
 }
